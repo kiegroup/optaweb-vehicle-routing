0e347a6b23e3ee65645748613acf9b22cdf86262
Jiří Locker <jiri.locker@gmail.com>
2018-08-01 15:31:26 +0200

    Add Place entity

---
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/Place.java
@@ -0,0 +1,35 @@
+package org.optaweb.tsp.optawebtspplanner;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/PlaceRepository.java
@@ -0,0 +1,7 @@
+package org.optaweb.tsp.optawebtspplanner;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaceRepository extends CrudRepository<Place, Long> {
+
+}
