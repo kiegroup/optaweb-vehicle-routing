628f09fe94c98b6b99dc2ca3847d453d21712ab5
Jiří Locker <jiri.locker@gmail.com>
2018-08-01 13:15:53 +0200

    Spring Initializr: Rest repository + JPA + H2

---
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/OptawebTspPlannerApplication.java
@@ -0,0 +1,12 @@
+package org.optaweb.tsp.optawebtspplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptawebTspPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptawebTspPlannerApplication.class, args);
+    }
+}
