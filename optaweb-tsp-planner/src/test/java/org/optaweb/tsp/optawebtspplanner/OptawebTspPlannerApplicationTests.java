17e3df8eef43db9996a6b0f920497f9e4ebecfee
Jiří Locker <jiri.locker@gmail.com>
2018-08-02 15:29:00 +0200

    Avoid scientific notation when serializing coordinates

--- optaweb-tsp-planner/src/test/java/org/optaweb/tsp/optawebtspplanner/OptawebTspPlannerApplicationTests.java
+++ optaweb-tsp-planner/src/test/java/org/optaweb/tsp/optawebtspplanner/OptawebTspPlannerApplicationTests.java
@@ -3,20 +3,34 @@ package org.optaweb.tsp.optawebtspplanner;
 import java.math.BigDecimal;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class OptawebTspPlannerApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -45,5 +59,32 @@ public class OptawebTspPlannerApplicationTests {
             softly.assertThat(response2.getBody().getLatitude()).isEqualTo(maxLatitude);
             softly.assertThat(response2.getBody().getLongitude()).isEqualTo(maxLongitude);
         });
+    }
+
+    @Test
+    public void should_create_entity() throws Exception {
+        mockMvc.perform(post("/places").content(
+                "{\"latitude\": \"1\", \"longitude\":\"2\"}")).andExpect(
+                status().isCreated()).andExpect(
+                header().string("Location", containsString("places/")));
+    }
+
+    @Test
+    @Ignore
+    public void values_close_to_zero_should_not_use_scientific_format() throws Exception {
+        // arrange
+        final String lat = "0.0000003";
+        final String lng = "0.0000005";
+        MvcResult mvcResult = mockMvc.perform(post("/places").content(
+                "{\"lat\": \"" + lat + "\", \"lng\":\"" + lng + "\"}"))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        // act & assert
+        String location = mvcResult.getResponse().getHeader("Location");
+        mockMvc.perform(get(location))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.lat").value(lat))
+                .andExpect(jsonPath("$.lng").value(lng));
     }
 }
