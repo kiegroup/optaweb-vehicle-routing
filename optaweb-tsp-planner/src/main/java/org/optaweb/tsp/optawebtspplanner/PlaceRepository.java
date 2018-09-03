423c7c77dd5e62944daea693d78d1567e0201b24
Jiří Locker <jiri.locker@gmail.com>
2018-09-03 17:04:35 +0200

    Post locations to server (with CORS)

--- optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/PlaceRepository.java
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/PlaceRepository.java
@@ -1,7 +1,9 @@
 package org.optaweb.tsp.optawebtspplanner;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface PlaceRepository extends CrudRepository<Place, Long> {
 
 }
