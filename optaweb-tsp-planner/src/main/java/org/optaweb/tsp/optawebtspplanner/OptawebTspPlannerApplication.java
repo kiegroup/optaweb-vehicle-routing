5eea30232292e8cf2a649da15088eacedb60eb47
Jiří Locker <jiri.locker@gmail.com>
2018-09-10 16:42:48 +0200

    Listen for best solution changed events

--- optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/OptawebTspPlannerApplication.java
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/OptawebTspPlannerApplication.java
@@ -2,10 +2,8 @@ package org.optaweb.tsp.optawebtspplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class OptawebTspPlannerApplication {
 
     public static void main(String[] args) {
