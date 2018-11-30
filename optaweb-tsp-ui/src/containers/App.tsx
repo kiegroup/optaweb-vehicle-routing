c862b9e0e3f0ac715d050561230e89aa595b3f8c
Daniele Fiungo <dfiungo@iad2.it>
2018-11-30 16:16:37 +0100

    Remove Tachyons dependency, Improve usage of Patternfly

--- optaweb-tsp-ui/src/containers/App.tsx
+++ optaweb-tsp-ui/src/containers/App.tsx
@@ -18,7 +18,6 @@ import * as React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import 'tachyons/css/tachyons.css';
 import TravelingSalesmanProblem, {
   ITravelingSalesmanProblemProps
 } from '../components/TravelingSalesmanProblem';
