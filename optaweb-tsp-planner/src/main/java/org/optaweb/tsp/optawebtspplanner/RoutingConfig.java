3b9d8eac2e388a6fc97127105c588591f6db29a2
Jiří Locker <jiri.locker@gmail.com>
2018-12-07 14:05:39 +0100

    Allow changing OSM file using a system property

--- optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/RoutingConfig.java
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/RoutingConfig.java
@@ -6,14 +6,26 @@ import com.graphhopper.reader.osm.GraphHopperOSM;
 import com.graphhopper.routing.util.EncodingManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class RoutingConfig {
 
-    private static Logger logger = LoggerFactory.getLogger(RoutingConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(RoutingConfig.class);
+    private static final String OSM_DIR = "local/openstreetmap/";
+    private static final String OSM_FILE_KEY = "osmfile";
+    private static final String GH_DIR = "local/graphhopper/";
+
+    private Environment env;
+
+    @Autowired
+    public RoutingConfig(Environment env) {
+        this.env = env;
+    }
 
     /**
      * Avoids creating real GraphHopper instance when running a @SpringBootTest.
@@ -23,13 +35,13 @@ public class RoutingConfig {
     @Bean
     public GraphHopperOSM graphHopper() {
         GraphHopperOSM graphHopper = ((GraphHopperOSM) new GraphHopperOSM().forServer());
-        String osmPath = "local/belgium-latest.osm.pbf";
+        String osmPath = OSM_DIR + env.getProperty(OSM_FILE_KEY, "belgium-latest.osm.pbf");
         if (!new File(osmPath).exists()) {
             throw new IllegalStateException("The osmPath (" + new File(osmPath).getAbsolutePath() + ") does not exist.\n" +
                     "Download the osm file from http://download.geofabrik.de/ first.");
         }
         graphHopper.setOSMFile(osmPath);
-        graphHopper.setGraphHopperLocation("local/" + osmPath.replaceFirst(".*/(.*)\\.osm\\.pbf$", "$1-gh"));
+        graphHopper.setGraphHopperLocation(GH_DIR + osmPath.replaceFirst(".*/(.*)\\.osm\\.pbf$", "$1"));
         graphHopper.setEncodingManager(new EncodingManager("car"));
         logger.info("GraphHopper loading...");
         graphHopper.importOrLoad();
