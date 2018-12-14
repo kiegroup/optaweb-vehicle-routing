4a0a184152e0e3f26124bc196836183c5a6e9f66
Jiří Locker <jiri.locker@gmail.com>
2018-12-14 13:59:00 +0100

    Javadoc: core

---
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains business objects and rules. The code in this package only depends on {@link java.lang} and is not affected
+ * by any framework used in this project.
+ */
+package org.optaweb.tsp.optawebtspplanner.core;
