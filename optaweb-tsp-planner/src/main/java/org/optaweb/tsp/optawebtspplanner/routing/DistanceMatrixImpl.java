49611bbdca57c46ea35dfb4b55dcd0da9818a7ca
Jiří Locker <jiri.locker@gmail.com>
2018-12-19 21:10:58 +0100

    Eliminate interactor -> routing dependency

--- optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/routing/DistanceMatrixImpl.java
+++ optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/routing/DistanceMatrixImpl.java
@@ -4,37 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.optaweb.tsp.optawebtspplanner.core.Location;
-import org.optaweb.tsp.optawebtspplanner.planner.DistanceMap;
+import org.optaweb.tsp.optawebtspplanner.interactor.DistanceMatrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * Keeps the information about distances between every pair of locations.
- * <p>
- * TODO Currently, the API is encumbered by usage of OptaPlanner VRP example code that works with
- * {@code Map<RoadLocation, Double>}.
  */
 @Component
-public class DistanceMatrix {
+public class DistanceMatrixImpl implements DistanceMatrix {
 
     private final RoutingComponent routing;
-    private final Map<Location, DistanceMap> matrix = new HashMap<>();
+    private final Map<Location, Map<Long, Double>> matrix = new HashMap<>();
 
     @Autowired
-    public DistanceMatrix(RoutingComponent routing) {
+    public DistanceMatrixImpl(RoutingComponent routing) {
         this.routing = routing;
     }
 
-    // TODO get rid of dependency on planner
-    public synchronized DistanceMap addLocation(Location location) {
-        DistanceMap distanceMap = new DistanceMap(location);
+    @Override
+    public synchronized void addLocation(Location location) {
+        Map<Long, Double> distanceMap = new HashMap<>();
         distanceMap.put(location.getId(), 0.0);
-        for (Map.Entry<Location, DistanceMap> entry : matrix.entrySet()) {
+        for (Map.Entry<Location, Map<Long, Double>> entry : matrix.entrySet()) {
             Location other = entry.getKey();
             distanceMap.put(other.getId(), routing.getDistance(location.getLatLng(), other.getLatLng()));
             entry.getValue().put(location.getId(), routing.getDistance(other.getLatLng(), location.getLatLng()));
         }
         matrix.put(location, distanceMap);
-        return distanceMap;
+    }
+
+    @Override
+    public synchronized Map<Long, Double> getRow(Location location) {
+        return matrix.get(location);
     }
 }
