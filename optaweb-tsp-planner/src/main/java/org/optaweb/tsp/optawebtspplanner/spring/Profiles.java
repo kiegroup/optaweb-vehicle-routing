fab3581ac211f9aad4f72de05a67bf76ee3c4507
Jiří Locker <jiri.locker@gmail.com>
2018-12-20 13:24:55 +0100

    Move Profiles to root package

--- optaweb-tsp-planner/src/main/java/org/optaweb/tsp/optawebtspplanner/spring/Profiles.java
+++
@@ -1,7 +0,0 @@
-package org.optaweb.tsp.optawebtspplanner.spring;
-
-public class Profiles {
-
-    public static final String TEST = "test";
-    public static final String NOT_TEST = "!" + TEST;
-}
