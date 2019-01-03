b6271a0ea678a3234d2f71d817d4a1ccd191e935
Jiří Locker <jiri.locker@gmail.com>
2019-01-03 13:21:46 +0100

    Add LocationRepository test

---
+++ optaweb-vehicle-routing-backend/src/test/java/org/optaweb/vehiclerouting/plugin/persistence/LocationRepositoryImplTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.vehiclerouting.plugin.persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.optaweb.vehiclerouting.domain.LatLng;
+import org.optaweb.vehiclerouting.domain.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class LocationRepositoryImplTest {
+
+    @Autowired
+    private LocationCrudRepository crudRepository;
+    private LocationRepositoryImpl repository;
+
+    @Before
+    public void setUp() {
+        repository = new LocationRepositoryImpl(crudRepository);
+    }
+
+    @Test
+    public void remove_created_location() {
+        LatLng latLng = LatLng.valueOf(0.00213, 32.777);
+        assertThat(crudRepository.count()).isZero();
+        Location location = repository.createLocation(latLng);
+        assertThat(location.getLatLng()).isEqualTo(latLng);
+        assertThat(crudRepository.count()).isOne();
+
+        Location removed = repository.removeLocation(location.getId());
+        assertThat(removed).isEqualTo(location);
+        assertThatThrownBy(() -> repository.removeLocation(location.getId()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
