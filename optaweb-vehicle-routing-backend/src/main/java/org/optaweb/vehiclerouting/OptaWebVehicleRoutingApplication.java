bf58355b920cbd04f4d99a38342a12c827b89d8f
Jiří Locker <jiri.locker@gmail.com>
2019-01-03 17:13:00 +0100

    Rename backend to optaweb-vehicle-routing-backend (#45)

--- optaweb-vehicle-routing-backend/src/main/java/org/optaweb/vehiclerouting/OptaWebVehicleRoutingApplication.java
+++ optaweb-vehicle-routing-backend/src/main/java/org/optaweb/vehiclerouting/OptaWebVehicleRoutingApplication.java
@@ -20,9 +20,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OptawebTspPlannerApplication {
+public class OptaWebVehicleRoutingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OptawebTspPlannerApplication.class, args);
+        SpringApplication.run(OptaWebVehicleRoutingApplication.class, args);
     }
 }
