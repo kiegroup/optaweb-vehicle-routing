70bd85f2f9ebdb97fc04715d39306d316fdbe737
Jiří Locker <jiri.locker@gmail.com>
2019-03-01 17:31:39 +0100

    Add basic implementation of the Visits page

--- optaweb-vehicle-routing-frontend/src/routes/Visits.tsx
+++ optaweb-vehicle-routing-frontend/src/routes/Visits.tsx
@@ -14,11 +14,76 @@
  * limitations under the License.
  */
 
+import { Text, TextContent, TextVariants } from '@patternfly/react-core';
 import * as React from 'react';
-import { WorkInProgress } from 'src/components/WorkInProgress';
+import { connect } from 'react-redux';
+import LocationList from '../components/LocationList';
+import { IAppState } from '../store/configStore';
+import { demoOperations } from '../store/demo';
+import { routeOperations, routeSelectors } from '../store/route';
+import { IRoute } from '../store/route/types';
 
-const Visits = () => {
-  return <WorkInProgress />;
+interface IStateProps {
+  route: IRoute;
+  domicileId: number;
+  isDemoLoading: boolean;
+}
+
+const mapStateToProps = ({ route, demo }: IAppState): IStateProps => ({
+  domicileId: routeSelectors.getDomicileId(route),
+  isDemoLoading: demo.isLoading,
+  route,
+});
+
+export interface IDispatchProps {
+  removeHandler: typeof routeOperations.deleteLocation;
+  loadHandler: typeof demoOperations.loadDemo;
+  clearHandler: typeof routeOperations.clearRoute;
+  addHandler: typeof routeOperations.addLocation;
+}
+
+const mapDispatchToProps: IDispatchProps = {
+  addHandler: routeOperations.addLocation,
+  clearHandler: routeOperations.clearRoute,
+  loadHandler: demoOperations.loadDemo,
+  removeHandler: routeOperations.deleteLocation,
 };
 
-export default Visits;
+type IProps = IStateProps & IDispatchProps;
+
+class Visits extends React.Component<IProps> {
+
+  constructor(props: IProps) {
+    super(props);
+  }
+
+  render(): React.ReactNode {
+    const {
+      route,
+      domicileId,
+      removeHandler,
+      loadHandler,
+      clearHandler,
+      isDemoLoading,
+    } = this.props;
+    return (
+      <React.Fragment>
+        <TextContent>
+          <Text component={TextVariants.h1}>Visits ({route.locations.length})</Text>
+        </TextContent>
+        {/* TODO do not show depots */}
+        <LocationList
+          removeHandler={removeHandler}
+          selectHandler={() => undefined}
+          loadHandler={loadHandler}
+          clearHandler={clearHandler}
+          route={route}
+          domicileId={domicileId}
+          isDemoLoading={isDemoLoading}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Visits);
