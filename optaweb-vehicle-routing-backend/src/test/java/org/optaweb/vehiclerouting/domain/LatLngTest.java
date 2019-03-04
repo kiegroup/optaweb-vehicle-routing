ec2eb579df21cc9b357be77c98e45ec53dc7249e
Jiří Locker <jiri.locker@gmail.com>
2019-03-04 22:32:32 +0100

    Improve domain and persistence test coverage

--- optaweb-vehicle-routing-backend/src/test/java/org/optaweb/vehiclerouting/domain/LatLngTest.java
+++ optaweb-vehicle-routing-backend/src/test/java/org/optaweb/vehiclerouting/domain/LatLngTest.java
@@ -33,9 +33,30 @@ public class LatLngTest {
 
     @Test
     public void latngs_should_be_equals_when_numerically_equal() {
+        LatLng latLng = new LatLng(BigDecimal.valueOf(987.1234), BigDecimal.valueOf(-0.1111));
+        assertThat(latLng).isEqualTo(latLng);
+
         BigDecimal ONE_POINT_ZERO = new BigDecimal("1.0");
         BigDecimal MINUS_ZERO = BigDecimal.ZERO.negate();
-        assertThat(new LatLng(MINUS_ZERO, ONE_POINT_ZERO))
-                .isEqualTo(new LatLng(BigDecimal.ZERO, BigDecimal.ONE));
+        assertThat(new LatLng(MINUS_ZERO, ONE_POINT_ZERO)).isEqualTo(new LatLng(BigDecimal.ZERO, BigDecimal.ONE));
+        assertThat(new LatLng(ONE_POINT_ZERO, MINUS_ZERO)).isEqualTo(new LatLng(BigDecimal.ONE, BigDecimal.ZERO));
+    }
+
+    @Test
+    public void should_not_equal() {
+        LatLng latLng = new LatLng(BigDecimal.ONE, BigDecimal.TEN);
+        assertThat(latLng).isNotEqualTo(null);
+        assertThat(latLng).isNotEqualTo(BigDecimal.valueOf(11));
+        assertThat(latLng).isNotEqualTo(new LatLng(BigDecimal.ONE, BigDecimal.ONE));
+        assertThat(latLng).isNotEqualTo(new LatLng(BigDecimal.TEN, BigDecimal.TEN));
+    }
+
+    @Test
+    public void value_of_and_getters() {
+        double latitude = Math.E;
+        double longitude = Math.PI;
+        LatLng latLng = LatLng.valueOf(latitude, longitude);
+        assertThat(latLng.getLatitude()).isEqualTo(BigDecimal.valueOf(latitude));
+        assertThat(latLng.getLongitude()).isEqualTo(BigDecimal.valueOf(longitude));
     }
 }
---
+++ optaweb-vehicle-routing-backend/src/test/java/org/optaweb/vehiclerouting/plugin/persistence/LocationEntityTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.vehiclerouting.plugin.persistence;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LocationEntityTest {
+
+    @Test
+    public void constructor_params_must_not_be_null() {
+        assertThatThrownBy(() -> new LocationEntity(null, BigDecimal.ZERO)).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> new LocationEntity(BigDecimal.ZERO, null)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void getters() {
+        BigDecimal latitude = BigDecimal.valueOf(0.101);
+        BigDecimal longitude = BigDecimal.valueOf(101.0);
+        LocationEntity locationEntity = new LocationEntity(latitude, longitude);
+        assertThat(locationEntity.getId()).isZero();
+        assertThat(locationEntity.getLongitude()).isEqualTo(longitude);
+        assertThat(locationEntity.getLatitude()).isEqualTo(latitude);
+    }
+}
