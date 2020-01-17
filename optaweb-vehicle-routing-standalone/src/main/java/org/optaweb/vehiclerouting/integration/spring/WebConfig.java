9c19522447447cfc4cfe4ae7d6f70e6f83926699
Jiří Locker <jiri.locker@gmail.com>
2020-01-17 01:47:50 +0100

    Add Spring Web config to support client side routing

---
+++ optaweb-vehicle-routing-standalone/src/main/java/org/optaweb/vehiclerouting/integration/spring/WebConfig.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.vehiclerouting.integration.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward requests without file extensions (e.g. /vehicles, but not /assets/images/optaplanner.png)
+        // to /index.html.
+        // In this case "page" is a variable name that is not used but is necessary to enable regexp matching.
+        registry
+                .addViewController("/{page:\\w+}")
+                .setViewName("forward:/");
+    }
+}
