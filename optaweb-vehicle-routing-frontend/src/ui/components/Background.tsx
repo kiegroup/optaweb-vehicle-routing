c1b88ce4f574be8ece4ddf78f7734fc2d9ccb3c1
Jiří Locker <jiri.locker@gmail.com>
2020-04-22 14:20:59 +0200

    Get rid of camelcase issues

--- optaweb-vehicle-routing-frontend/src/ui/components/Background.tsx
+++ optaweb-vehicle-routing-frontend/src/ui/components/Background.tsx
@@ -16,21 +16,19 @@
 
 import { BackgroundImage, BackgroundImageSrc } from '@patternfly/react-core';
 import filter from '@patternfly/react-core/dist/styles/assets/images/background-filter.svg';
-/* eslint-disable camelcase */
-import pfbg_1200 from '@patternfly/react-core/dist/styles/assets/images/pfbg_1200.jpg';
-import pfbg_576 from '@patternfly/react-core/dist/styles/assets/images/pfbg_576.jpg';
-import pfbg_576_2x from '@patternfly/react-core/dist/styles/assets/images/pfbg_576@2x.jpg';
-import pfbg_768 from '@patternfly/react-core/dist/styles/assets/images/pfbg_768.jpg';
-import pfbg_768_2x from '@patternfly/react-core/dist/styles/assets/images/pfbg_768@2x.jpg';
-import * as React from 'react';
-/* eslint-enable camelcase */
+import pfBackground1200 from '@patternfly/react-core/dist/styles/assets/images/pfbg_1200.jpg';
+import pfBackground576 from '@patternfly/react-core/dist/styles/assets/images/pfbg_576.jpg';
+import pfBackground1152 from '@patternfly/react-core/dist/styles/assets/images/pfbg_576@2x.jpg';
+import pfBackground768 from '@patternfly/react-core/dist/styles/assets/images/pfbg_768.jpg';
+import pfBackground1536 from '@patternfly/react-core/dist/styles/assets/images/pfbg_768@2x.jpg';
+import React from 'react';
 
 const bgImages = {
-  [BackgroundImageSrc.lg]: pfbg_1200,
-  [BackgroundImageSrc.sm]: pfbg_768,
-  [BackgroundImageSrc.sm2x]: pfbg_768_2x,
-  [BackgroundImageSrc.xs]: pfbg_576,
-  [BackgroundImageSrc.xs2x]: pfbg_576_2x,
+  [BackgroundImageSrc.xs]: pfBackground576,
+  [BackgroundImageSrc.sm]: pfBackground768,
+  [BackgroundImageSrc.xs2x]: pfBackground1152,
+  [BackgroundImageSrc.lg]: pfBackground1200,
+  [BackgroundImageSrc.sm2x]: pfBackground1536,
   [BackgroundImageSrc.filter]: `${filter}#image_overlay`,
 };
 
