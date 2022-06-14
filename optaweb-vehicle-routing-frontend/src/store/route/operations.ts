42c56e6bc1ae9943025c39f7801fb33a2876d340
Jiří Locker <jiri.locker@gmail.com>
2022-06-14 17:05:24 +0200

    Rename unused function parameters
    Detected by SonarCloud.
    
    "Unused parameters are misleading. ...the rule ignores all parameters
    whose name starts with an underscore (_). This is a common practice to
    acknowledge the fact that some parameter is unused (e.g. in TypeScript
    compiler)."

--- optaweb-vehicle-routing-frontend/src/store/route/operations.ts
+++ optaweb-vehicle-routing-frontend/src/store/route/operations.ts
@@ -13,41 +13,41 @@ import {
 export const { updateRoute } = actions;
 
 export const addLocation: ThunkCommandFactory<LatLngWithDescription, AddLocationAction> = (
-  (location) => (dispatch, getState, client) => {
+  (location) => (dispatch, _getState, client) => {
     dispatch(actions.addLocation(location));
     client.addLocation(location);
   });
 
 export const deleteLocation: ThunkCommandFactory<number, DeleteLocationAction> = (
-  (locationId) => (dispatch, getState, client) => {
+  (locationId) => (dispatch, _getState, client) => {
     dispatch(actions.deleteLocation(locationId));
     client.deleteLocation(locationId);
   });
 
 export const addVehicle: ThunkCommandFactory<void, AddVehicleAction> = (
-  () => (dispatch, getState, client) => {
+  () => (dispatch, _getState, client) => {
     dispatch(actions.addVehicle());
     client.addVehicle();
   });
 
 export const deleteVehicle: ThunkCommandFactory<number, DeleteVehicleAction> = (
-  (vehicleId) => (dispatch, getState, client) => {
+  (vehicleId) => (dispatch, _getState, client) => {
     dispatch(actions.deleteVehicle(vehicleId));
     client.deleteVehicle(vehicleId);
   });
 
 export const deleteAnyVehicle: ThunkCommandFactory<void, never> = (
-  () => (dispatch, getState, client) => {
+  () => (_dispatch, _getState, client) => {
     client.deleteAnyVehicle();
   });
 
 export const changeVehicleCapacity: ThunkCommandFactory<VehicleCapacity, never> = (
-  ({ vehicleId, capacity }: VehicleCapacity) => (dispatch, getState, client) => {
+  ({ vehicleId, capacity }: VehicleCapacity) => (_dispatch, _getState, client) => {
     client.changeVehicleCapacity(vehicleId, capacity);
   });
 
 export const clearRoute: ThunkCommandFactory<void, ClearRouteAction> = (
-  () => (dispatch, getState, client) => {
+  () => (dispatch, _getState, client) => {
     dispatch(actions.clearRoute());
     client.clear();
   });
