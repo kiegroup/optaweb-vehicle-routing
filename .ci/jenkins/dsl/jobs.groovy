fbf5ef7a74646a679059cb1f0c255e0a23665547
radtriste <tradisso@redhat.com>
2022-08-24 13:47:59 +0200

    KOGITO-7786 Setup init-branch jobs

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -9,7 +9,11 @@
 */
 
 import org.kie.jenkins.jobdsl.model.Environment
+import org.kie.jenkins.jobdsl.model.Folder
+import org.kie.jenkins.jobdsl.KogitoJobTemplate
 import org.kie.jenkins.jobdsl.KogitoJobUtils
+
+jenkins_path = '.ci/jenkins'
 
 Map getMultijobPRConfig() {
     return [
@@ -28,3 +32,34 @@ List environments = Environment.getActiveEnvironments(this)
 environments.retainAll { it != Environment.NATIVE } // There is no requirement for Native support.
 environments.retainAll { it != Environment.MANDREL } // There is no requirement for Native Mandrel support.
 KogitoJobUtils.createPerEnvPerRepoPRJobs(this, environments) { jobFolder -> getMultijobPRConfig() }
+
+// Init branch
+setupInitBranchJob()
+
+
+void setupInitBranchJob() {
+    def jobParams = KogitoJobUtils.getBasicJobParams(this, 'optaweb-vehicle-routing', Folder.INIT_BRANCH, "${jenkins_path}/Jenkinsfile.init-branch", 'Optaweb Vehicle Routing Init branch')
+    KogitoJobUtils.setupJobParamsDefaultMavenConfiguration(this, jobParams)
+    jobParams.env.putAll([
+        REPO_NAME: 'optaweb-vehicle-routing',
+        JENKINS_EMAIL_CREDS_ID: "${JENKINS_EMAIL_CREDS_ID}",
+
+        GIT_AUTHOR: "${GIT_AUTHOR_NAME}",
+        AUTHOR_CREDS_ID: "${GIT_AUTHOR_CREDENTIALS_ID}",
+
+        MAVEN_SETTINGS_CONFIG_FILE_ID: "${MAVEN_SETTINGS_FILE_ID}",
+        MAVEN_DEPENDENCIES_REPOSITORY: "${MAVEN_ARTIFACTS_REPOSITORY}",
+        MAVEN_DEPLOY_REPOSITORY: "${MAVEN_ARTIFACTS_REPOSITORY}",
+    ])
+    KogitoJobTemplate.createPipelineJob(this, jobParams)?.with {
+        parameters {
+            stringParam('DISPLAY_NAME', '', 'Setup a specific build display name')
+
+            stringParam('BUILD_BRANCH_NAME', "${GIT_BRANCH}", 'Set the Git branch to checkout')
+
+            stringParam('OPTAPLANNER_VERSION', '', 'Optaplanner version to set.')
+
+            booleanParam('SEND_NOTIFICATION', false, 'In case you want the pipeline to send a notification on CI channel for this run.')
+        }
+    }
+}
