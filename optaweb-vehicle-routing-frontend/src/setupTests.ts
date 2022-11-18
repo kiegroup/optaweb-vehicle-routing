4500362d4d612120e545657dc17229883c646f8a
Jiří Locker <jiri.locker@gmail.com>
2022-11-18 19:32:08 +0100

    Use @wojtekmaj/enzyme-adapter-react-17

--- optaweb-vehicle-routing-frontend/src/setupTests.ts
+++ optaweb-vehicle-routing-frontend/src/setupTests.ts
@@ -1,8 +1,8 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import EventSource from 'eventsourcemock';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 Object.defineProperty(window, 'EventSource', {
   value: EventSource,
